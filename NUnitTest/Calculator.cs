c29a6161980443596a0c23ddab0703434c298579
kirillparaschenko <kparaschenko@caeonline.com>
2023-04-13 18:23:51 +0300

    Completed HW, Added Class Calculator, added test class

---
+++ NUnitTest/Calculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NUnitTest
+{
+    public class Calculator
+    {
+        public double Summ (int operand1, int operand2) => operand1 + operand2;
+        public double Minus(int operand1, int operand2) => operand1 - operand2;
+        public double Multiply(int operand1, int operand2) => operand1 * operand2;
+        public double Devide(int operand1, int operand2) => operand1 / operand2;
+    }
+}
